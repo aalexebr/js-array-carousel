8ac598a953a21099eb62422098c8200b4eb880c6
aalexebr <alex.ebr@icloud.com>
2023-06-16 14:29:27 +0200

    solution for img in array but not the best

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// ora si piange
+
+// array of images
+let imgArray = []
+imgArray[0] = new Image();
+imgArray[0].src = 'img/01.webp';
+imgArray[1] = new Image();
+imgArray[1].src = 'img/02.webp';
+imgArray[2] = new Image();
+imgArray[2].src = 'img/03.webp';
+imgArray[3] = new Image();
+imgArray[3].src = 'img/04.webp';
+imgArray[4] = new Image();
+imgArray[4].src = 'img/05.webp';
+
+// imgArray.push('img/02.webp')
+// imgArray.push('img/03.webp')
+// imgArray.push('img/04.webp')
+// imgArray.push('img/05.webp')
+
+console.log(imgArray)
+
+// create .box div
+let box = document.createElement('div')
+box.classList.add('box')
+document.getElementById('position').append(box)
+
+
+// create <img>
+let img = document.createElement('img')
+img.src = imgArray[0].src;
+box.append(img)
