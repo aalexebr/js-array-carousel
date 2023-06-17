792411fcf14f4c78cf9cc8d860b57d4068a107e6
aalexebr <alex.ebr@icloud.com>
2023-06-17 20:21:14 +0200

    slow progress

--- js/script.js
+++ js/script.js
@@ -1,65 +1,72 @@
 // ora si piange
 
 // array of images
-let imgArray = []
-imgArray[0] = new Image();
-imgArray[0].src = 'img/01.webp';
-imgArray[1] = new Image();
-imgArray[1].src = 'img/02.webp';
-imgArray[2] = new Image();
-imgArray[2].src = 'img/03.webp';
-imgArray[3] = new Image();
-imgArray[3].src = 'img/04.webp';
-imgArray[4] = new Image();
-imgArray[4].src = 'img/05.webp';
+const imgArray = []
 
 
 
-// create .box div
-// const box = document.createElement('div')
-// box.classList.add('box')
-// document.getElementById('position').append(box)
+// i=1
+const box = document.createElement("div");
+box.classList.add('box')
+document.getElementById('position').append(box)
+// document.querySelector('.box').innerHTML += `<img src='${imgArray[i]}'> `
 
+for (i=0; i <5; i++){
+    imgArray[i] = `img/0${i+1}.webp`
 
-// create <img> (check to work)
-// const img = document.createElement('img')
-// img.src =imgArray[0].src;
-// box.append(img)
-const up = document.getElementById('up')
-const down = document.getElementById('down')
-
-// cycle
-//             img.src = imgArray[0].src;
-//             box.append(img)
-//             box.classList.add('display')
-//             let scope = 0
-// down.addEventListener('click',
-//     function(){
-//         scope = 1
-//         for (let i = 0; i<imgArray.length; i++){
-            
-//             if(scope =1){
-//                 img.src = imgArray[(i)].src;
-//                 box.append(img)
-//                 box.classList.add('display')
-
-//                 console.log(imgArray[i])
-            
-//             }  
-            
-
-//         }
-//     }   
-// )
-
-for (let i=0; i<imgArray.length; i++){
-
-
-    const box = document.createElement('div')
-    box.classList.add('box')
-    document.getElementById('position').append(box)
-    let img = document.createElement('img')
-    img.src = imgArray[(i)].src;
-    box.append(img)
-    box.classList.add('display')
+    document.querySelector('.box').innerHTML += `<img src='${imgArray[i]}' class='display'> `
 }
+
+console.log(i)
+console.log(imgArray)
+
+
+const down = document.getElementById('down')
+let count = 1
+document.querySelector(`img:nth-of-type(${count+1})`)
+console.log(document.querySelector(`img:nth-of-type(${count+1})`))
+down.addEventListener('click', function(){
+    if(count < imgArray.length){
+        count ++
+    }
+    else{
+        count=0
+    }
+    // document.querySelector(`img:nth-of-type(${count})`).classList.add('display')
+    }
+)
+// console.log(document.querySelector(`img:nth-of-type()`))
+// for i=1 to <=5
+//     imgArray[i] = `img/0${i}.webp`
+//     if(i == 1) 
+
+//     document.querySelector('.containerImmagini').innerHTML += `<img src="img/0${i}.webp" class="active">`
+
+
+// const up = document.getElementById('up')
+// const down = document.getElementById('down')
+
+// let count = 1
+
+// down.addEventListener('click', function(){
+//     if(count < numero immagini)
+//         count++
+//     else
+//         count = 0
+//     document.querySelector(`.containerImmagini > img:nth-chidl(${count+1})`).classList.add('active')
+    
+//     img.src = imgArray[count]
+// })
+
+
+
+// imgArray[0] = new Image();
+// imgArray[0].src = 'img/01.webp';
+// imgArray[1] = new Image();
+// imgArray[1].src = 'img/02.webp';
+// imgArray[2] = new Image();
+// imgArray[2].src = 'img/03.webp';
+// imgArray[3] = new Image();
+// imgArray[3].src = 'img/04.webp';
+// imgArray[4] = new Image();
+// imgArray[4].src = 'img/05.webp';
