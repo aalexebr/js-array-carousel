6696da5b470971f5fec58e034be5303eefde5164
aalexebr <alex.ebr@icloud.com>
2023-06-18 20:13:54 +0200

    stuck in count problem

--- js/script.js
+++ js/script.js
@@ -19,23 +19,69 @@ for (i=0; i <5; i++){
 
 console.log(i)
 console.log(imgArray)
+console.log(imgArray[0])
 const down = document.getElementById('down')
 
 let count = 0
 document.querySelector(`img:nth-of-type(${count+1})`).classList.add('display')
 
 console.log(document.querySelector(`img:nth-of-type(${count+1})`))
+
+console.log(count)
 down.addEventListener('click', function(){
+    count ++
+    document.querySelector(`img:nth-of-type(${count})`).classList.remove('display')
+    
     if(count < imgArray.length){
-        count ++
-        document.querySelector(`img:nth-of-type(${count})`).classList.remove('display')
+           
     }
     else{
-        count=0
-    }
+        count = 0
+        
+    }  
     document.querySelector(`img:nth-of-type(${count+1})`).classList.add('display')
+    console.log(count)
     }
 )
+
+const up = document.getElementById('up')
+let count2 = 0
+up.addEventListener('click', function(){
+    count2 --
+    document.querySelector(`img:nth-of-type(${count2})`).classList.remove('display')
+    
+    if(count2 < imgArray.length){
+        
+    console.log(count2)
+    
+        
+    }
+    else{
+        count2 = 0
+        console.log(count2)
+    }
+    
+    document.querySelector(`img:nth-of-type(${count2+1})`).classList.add('display')
+
+    }
+)
+
+// down.addEventListener('click', function(){
+//     count --
+//     console.log(count)
+//     document.querySelector(`img:nth-of-type(${count})`).classList.remove('display')
+    
+    
+//     if(count < imgArray.length){
+        
+        
+//     }
+//     else{
+//         count=0
+//     }
+//     document.querySelector(`img:nth-of-type(${count+1})`).classList.add('display')
+//     }
+// )
 // console.log(document.querySelector(`img:nth-of-type()`))
 // for i=1 to <=5
 //     imgArray[i] = `img/0${i}.webp`
